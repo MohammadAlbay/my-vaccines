7affcac5ed47c42d7dd31261518df573ad995006
Mohammad s. Albay <43953576+MohammadAlbay@users.noreply.github.com>
2024-11-15 01:53:13 +0200

    Added models & migrations

---
+++ app/Models/HealthcareVaccineLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HealthcareVaccineLog extends Model
+{
+    //
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My vaccines</title>
+</head>
+<body>
+    <h1>My Vaccines System</h1>
+    <hr>
+</body>
+</html>
